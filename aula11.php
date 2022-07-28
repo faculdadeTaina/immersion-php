11137e56c9e7f5fd53d6f939c146ca52f20d3a70
Tainá Miranda <Tainamirandaferreira@gmail.com>
2022-07-28 23:56:50 +0000

    funções de data

--- aula11.php
+++ aula11.php
@@ -2,5 +2,13 @@
 $num=7.3;
 //arrendondar um valor pra cima
 echo ceil($num);
+echo '<br/>';
+//arrendondar um valor pra baixo
+echo floor($num);
+echo '<br/>';
+// um valor aleario
+echo rand(10, 20);
+//echo gettrandmax();
+
 
 ?>
---
+++ aula12.php
@@ -0,0 +1,16 @@
+<?php
+//manipula datas
+//data aual
+echo date('d');
+
+echo '<br/>';
+//dia atual
+echo date('D');
+echo '<br/>';
+//ano/mes/ano atual
+echo date('d/m/Y');
+echo '<br/>';
+//descobrir o timezone
+
+?>
+
