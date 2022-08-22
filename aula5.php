ed6f548b37d24a4e99f317b599f14411965d1653
Tainá Miranda <Tainamirandaferreira@gmail.com>
2022-08-22 18:41:01 +0000

    herança

--- aula5.php
+++ aula5.php
@@ -6,16 +6,24 @@ class Pessoa{
 
   //construtor 
   function __construct($nome){
-    echo 'Objeto iniciado';
+    //echo 'Objeto iniciado';
     $this->nome=$nome;
   }
 
+  function __destructor(){
+    echo 'objeto removido';
+  }
+
   function __get($atributo){
+    return $this-> $atributo;
     
   }
 
 }
 
 $pessoa=new Pessoa('Jorge');
+echo $pessoa -> __get('nome');
+
+unset($pessoa);
 
 ?>
