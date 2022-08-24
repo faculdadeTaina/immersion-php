a31dac7feca99f93a7b37df3aa2682e411d0f72e
Tainá Miranda <Tainamirandaferreira@gmail.com>
2022-08-24 18:44:25 +0000

    aula finalizada

--- aula8.php
+++ aula8.php
@@ -7,10 +7,31 @@ class Pai{
   protected $sobrenome='Silva';
   public $humor='Feliz';
 
+  public function getSobrenome(){
+    return $this->sobrenome;
+  }
+
+   public function setSobrenome(){
+    if(strlen($value)>=3){
+      $this->sobrenome=$value;
+    }
+  }
 
 }
 
-$pai = new Pai();
+/*$pai = new Pai();
 echo $pai ->humor;
+$pai ->humor='Triste';
+echo '<br/>';
+echo $pai ->humor;*/
+$pai = new Pai();
+
+echo $pai ->getSobrenome();
+
+$pai->setSobrenome('Oliveira');
+
+echo '<br/>';
+
+echo $pai->getSobrenome();
 
 ?>
