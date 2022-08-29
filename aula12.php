4156be53fb147e35a5d9663c904029c9f212f50b
Tainá Miranda <Tainamirandaferreira@gmail.com>
2022-08-29 19:14:50 +0000

    namespace

--- aula12.php
+++ aula12.php
@@ -1,9 +1,48 @@
 <?php
-//manipula datas
-_final .'-'.$timeF;
+//namespace
 
-$diferenca_t=$timeF-$timeI;
-echo $diferenca_t;
+//namespace com nome a
+namespace A;
 
+interface CadastroInterface{
+  public function salvar();
+}
+
+class Cliente implements CadastroInterface{
+  
+  public $nome="Felipe";
+  public function __get($attr){
+    return $this->$attr;
+    
+  }
+  public function salvar(){
+    echo 'salvar';
+  }
+  
+}
+
+//namespace com nome b
+namespace B;
+
+interface CadastroInterface {
+  public function remover();
+}
+
+
+class Cliente implements CadastroInterface{
+
+  
+  public $nome="Jorge";
+  public function __get($attr){
+    return $this->$attr;
+    
+  }
+   public function remover(){
+    echo 'remover';
+  }
+}
+
+$c= new Cliente();
+print_r($c);
+echo $c->__get('nome');
 ?>
-
